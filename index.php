b40306d5e34ffe0b15a153ba960c2101ab0d393e
belette <ouack23@yahoo.fr>
2016-06-20 20:04:31 +0200

    added echo to append_sid to make it ... work

--- index.php
+++ index.php
@@ -12,10 +12,10 @@ include ('include/functions.php');
 		<section id="main">
 			<h1>Accueil du site</h1>
 			<p>Yo, je te présente le site internet dédié à notre recherche de Maison / Ferme / Grange / etc ... Si tu t'inscris, tu vas pouvoir
-				<a href="<?php append_sid('annonces.php');?>">ajouter</a> des annonces intéressantes dans la base de données,
-				<a href="<?php append_sid('comments.php'); ?>">commenter</a> les annonces déjà publiées,
-				<a href="<?php append_sid('listeCR.php'); ?>">lire</a> les comptes-rendus de réunion, et poster des messages sur le
-				<a href="<?php append_sid('forum/index.php'); ?>">Forum</a>. N'hésite plus, rejoins-nous, on est bien.</p>
+				<a href="<?php echo(append_sid('annonces.php'));?>">ajouter</a> des annonces intéressantes dans la base de données,
+				<a href="<?php echo(append_sid('comments.php')); ?>">commenter</a> les annonces déjà publiées,
+				<a href="<?php echo(append_sid('listeCR.php')); ?>">lire</a> les comptes-rendus de réunion, et poster des messages sur le
+				<a href="<?php echo(append_sid('forum/index.php')); ?>">Forum</a>. N'hésite plus, rejoins-nous, on est bien.</p>
 			<p>Si tu te sens un peu trop babos et tu cherches quelqu'un qui te remette à ta place, tu peux écouter la musique ci-dessous.</p>
 			<iframe width="800" height="600" src="https://www.youtube.com/embed/Rq3SYvzm6c8" frameborder="0" allowfullscreen></iframe>
 		</section>
