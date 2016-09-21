949fd82fa8e193bac8874b14cce969016524f5c6
belette <ouack23@yahoo.fr>
2016-09-21 09:12:48 +0200

    Print limesurvey and booked links in menu if not in localhost

--- include/menu.php
+++ include/menu.php
@@ -1,9 +1,12 @@
 ﻿<nav>
 		<ul>
 			<?php
+			$host = $request->server('HTTP_HOST', '');
+			$is_localhost = $host == 'localhost';
+			
 			echo('<li><a href="'.append_sid('index.php').'">Accueil</a></li>');
 			echo('<li><a href="'.append_sid('forum/index.php').'">Forum</a></li>');
-
+			
 			if($user->data['is_registered']) {
 				echo('<li><a href="'.append_sid('docs.php').'">Documents</a></li>');
 				echo('<li><a href="'.append_sid('annonces.php', 'reverse=true').'">Annonces</a></li>');
@@ -11,7 +14,12 @@
 				echo('<li><a href="'.append_sid('forum/ucp.php').'">Mon Profil</a></li>');
 				echo('<li><a href="'.append_sid('forum/ucp.php', 'mode=logout', true, $user->session_id).'">Déconnexion</a></li>');
 				
-				if($user->data['username'] == 'Belette' && $request->server('HTTP_HOST', '') == 'localhost')
+				if(!$is_localhost) {
+					echo('<li><a href="'.append_sid('survey/index.php').'">Sondages</a></li>');
+					echo('<li><a href="'.append_sid('booked/index.php').'">Calendrier</a></li>');
+				}
+				
+				if($user->data['username'] == 'Belette' && $is_localhost)
 					echo('<li><a href="'.append_sid('phpmyadmin/').'">PMA</a></li>');
 			}
 			
