04b85cc9bbb690440c31c1c6079f2ac81dd9cb52
belette <ouack23@yahoo.fr>
2016-09-25 19:25:43 +0200

    Update menu to show LimeSurvey admin link to work group and admin members

--- include/menu.php
+++ include/menu.php
@@ -4,6 +4,13 @@
 			$host = $request->server('HTTP_HOST', '');
 			$is_localhost = $host == 'localhost';
 			
+			$admin = 'Belette';
+			$GdT = ['Éric', 'Buzz', 'Bastien', 'TimRocket', 'Zaza', 'Keks'];
+			
+			$is_admin = $user->data['username'] == $admin;
+			$is_in_GdT = in_array($user->data['username'], $GdT);
+			
+			echo('<ul>');
 			echo('<li><a href="'.append_sid('index.php').'">Accueil</a></li>');
 			echo('<li><a href="'.append_sid('forum/index.php').'">Forum</a></li>');
 			
@@ -15,17 +22,22 @@
 				if(!$is_localhost) {
 					echo('<li><a href="'.append_sid('survey/index.php').'">Sondages</a></li>');
 					echo('<li><a href="'.append_sid('booked/index.php').'">Calendrier</a></li>');
+					
+					if($is_admin || $is_in_GdT)
+						echo('<li><a href="'.append_sid('survey/admin/').'">Admin Sondages</a></li>');
 				}
 				
 				echo('<li><a href="'.append_sid('forum/ucp.php').'">Mon Profil</a></li>');
 				echo('<li><a href="'.append_sid('forum/ucp.php', 'mode=logout', true, $user->session_id).'">Déconnexion</a></li>');
 				
-				if($user->data['username'] == 'Belette' && $is_localhost)
+				if($is_admin && $is_localhost)
 					echo('<li><a href="'.append_sid('phpmyadmin/').'">PMA</a></li>');
 			}
 			
 			else
 				echo('<li><a href="'.append_sid('forum/ucp.php', 'mode=register', true, $user->session_id).'">Inscription</a></li>');
+			
+			echo('</ul>');
 			?>
 		</ul>
 </nav>
