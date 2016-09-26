099224d0977e18dbbd8fea30edf60240cb24e37f
belette <ouack23@yahoo.fr>
2016-09-26 18:27:52 +0200

    Refactor index page

--- index.php
+++ index.php
@@ -12,15 +12,20 @@ include('include/functions.php');
 		<?php include_content('top'); ?>
 		<section id="main">
 			<h1>Accueil du site</h1>
-			<p>Yo, je te présente le site internet dédié à notre recherche de Maison / Ferme / Grange / etc ... Si tu t'inscris, tu vas pouvoir
-				<a href="<?php echo(append_sid('annonces.php'));?>">ajouter</a> des annonces intéressantes dans la base de données,
-				<a href="<?php echo(append_sid('comments.php')); ?>">commenter</a> les annonces déjà publiées,
-				<a href="<?php echo(append_sid('docs.php')); ?>">lire</a> les comptes-rendus de réunion, et poster des messages sur le
-				<a href="<?php echo(append_sid('forum/index.php')); ?>">Forum</a>. N'hésite plus, rejoins-nous, on est bien !<br /><br />
-			</p>
+			
+			<h2>Dernières nouvelles</h2>
+			
+			<p>La création d'une association est nécessaire avant d'envisager tout investissement financier. Il nous faut donc définir des <em>statuts</em> et un <em>règlement intérieur</em>.<br /><br />
+			Après l'observation du fait que les discussions en groupe entier à ces sujets étaient bien trop longues, un travail a donc été constitué, autour des personnes suivantes : Keks, Zaza, Bastien, Éric, Koline, TimRocket et Ratich.<br />
+			Ils se sont engagés quant à leur disponibilité et leur travail actif à ces sujets administratifs d'une importance majeure. Leur but est de déblayer au maximum la question avant de soumettre leur premier jet de texte au reste du groupe.</p>
+			
+			<p>Nous devons aussi trouver un <a href="forum/viewtopic.php?f=7&t=3">nom à notre association</a> !</p>
+			
+			<p></p>
 			
 			<h2>Prochaine réunion</h2>
-			<p>Remplissez le <a href="https://framadate.org/TNsCagUIBPnns1XT">Framadate</a> de la prochaine réunion !</p>
+			
+			<p>Remplissez le <a href="https://framadate.org/TNsCagUIBPnns1XT">framadate</a> de la prochaine réunion !</p>
 		</section>
 		<?php include_content('bottom'); ?>
 	</body>
