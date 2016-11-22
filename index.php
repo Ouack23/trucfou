8457d03eeeb87c3cc1401a88308aea21a2145c9e
Ouack23 <ouack23@yahoo.fr>
2016-11-22 08:19:13 +0100

    Update index page

--- index.php
+++ index.php
@@ -19,14 +19,15 @@ include('include/functions.php');
 			Après l'observation du fait que les discussions en groupe entier à ces sujets étaient bien trop longues, un travail a donc été constitué, autour des personnes suivantes : Keks, Zaza, Bastien, Éric, Koline, TimRocket et Ratich.<br />
 			Ils se sont engagés quant à leur disponibilité et leur travail actif à ces sujets administratifs d'une importance majeure. Leur but est de déblayer au maximum la question avant de soumettre leur premier jet de texte au reste du groupe.</p>
 			
-			<p>Nous devons aussi trouver un <a href="forum/viewtopic.php?f=7&t=3">nom à notre association</a> !</p>
+			<p>Nous devons aussi trouver un <a href="forum/viewtopic.php?f=7&t=3">nom à notre association</a> !<br /></p>
 			
-			<p></p>
+			<h2>Prochaine réunion Grand Groupe</h2>
 			
-			<h2>Prochaine réunion (Novembre 2016)</h2>
+			<p>La prochaine réunion Grand Groupe aura lieu le <strong>Jeudi 1<sup>er</sup> Décembre</strong> à <strong>20h</strong>, dans un lieu qui reste encore à définir. Soyez motivés, tout plein de choses vont être discutaillées !!<br /></p>
 			
-			<p>Remplissez le <a href="https://framadate.org/reuniongrandgroupe">framadate</a> de la prochaine réunion ! Attention, pensez bien à remplir toutes, les dates, il faut cliquer sur
-			les petites flèches zob pour ne pas en oublier.<strong>DATE LIMITE LE 7 NOVEMBRE</strong></p>
+			<h2>Prochaines réunions Groupe de Travail</h2>
+			
+			<p>Les réunions Groupe de Travail ont lieu tous les mardis soirs à 20h dans une salle de la BMC. Tout le monde peut y participer. Pour être sûr du lieu, allez voir <a href="http://trucfou.pe.hu/forum/viewforum.php?f=19">ici</a>.</p>
 		</section>
 		<?php include_content('bottom'); ?>
 	</body>
