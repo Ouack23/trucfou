a0ca69f22afbbbcf864da095184f0506779b2dbb
FelixDz <felix.detrez@gmail.com>
2017-01-14 12:36:28 +0100

    Index.php structure + fixes
    Updated index.php's structure and added minor responsive fixes

--- index.php
+++ index.php
@@ -5,36 +5,80 @@ include('include/functions.php');
 	<head>
 		<meta charset="utf-8" />
 		<title>Un projet de malade</title>
+		<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 		<link rel="stylesheet" href="style.css" />
 		<link rel="icon" type="image/x-icon" href="favicon.ico" />
 	</head>
 	<body>
 		<?php include_content('top'); ?>
 		<section id="main">
-			<h1>Accueil du site</h1>
-			
-			<h2>Dernières nouvelles</h2>
-			
-			<p>La création d'une association est nécessaire avant d'envisager tout investissement financier. Il nous faut donc définir des <em>statuts</em> et un <em>règlement intérieur</em>.<br /><br/>
 
+			<h1 class="main-title">Accueil du site</h1>
 
-			Après l'observation du fait que les discussions en groupe entier à ces sujets étaient bien trop longues, un groupe de travail a donc été constitué, autour des personnes suivantes : Keks, <s>Zaza</s>, Bastien, Éric, Koline, TimRocket,<del> Ratich</del>, <del>Niko</del>, LeLukas, Fanny et <del>Belette</del>.<br />
-			Ils se sont engagés quant à leur disponibilité et leur travail actif à ces sujets administratifs d'une importance majeure. Leur but est de déblayer au maximum la question avant de soumettre leur premier jet de texte au reste du groupe.<br />
-			Leurs réunions sont évidemment accessibles à tous (voir section dédiée ci-dessous).<br/>
-			Les comptes rendus des réunions Groupe de Travail sont tous uploadés <a href="http://trucfou.pe.hu/forum/viewforum.php?f=18">ici</a>.</p>
-			
-			<p>Nous devons (entre autres considérations sémantiques) aussi trouver un <a href="forum/viewtopic.php?f=7&t=3">nom à notre association</a> !<br /></p>
-			
-			<h2>Prochaine réunion Grand Groupe <em>(également dite plénière)</em></h2>
-			
-			<p>La prochaine réunion Grand Groupe aura lieu à une date encore inconnue, située entre le 10 et le 27 du mois de janvier 2017, dans un lieu qui reste encore à définir. Soyez motivés, soyez chatoyants et donnez vos disponibilités sur <strong><a href="https://framadate.org/vlltexDcPDfH5pmkJcKylArP/admin">ce framazob !</a></strong><br /></p>
+			<div class="flex-container">
 
-<u><p> <em>Pour rappel, voici le sage message de notre cher LeLukas qui exprimait fort justement la pensée de tous : </em> </u><br/>
-<em>« En fait on s'est dit que faire une réunion à 12 c'était pas optireprésentatif et que nous n'en ferions plus à moins de 16 gens. A vos agenda les poulets ! Pensez à modifier vos disponibilités sur le framazobie si vos disponibilités changent. »</em></p>
-			
-			<h2>Prochaines réunions Groupe de Travail</h2>
-			
-			<p>Les réunions Groupe de Travail ont lieu tous les mardis soirs à 20h dans une salle de la BMC. Tout le monde peut y participer. Pour être sûr du lieu, allez voir <a href="http://trucfou.pe.hu/forum/viewforum.php?f=19">ici</a>.</p>
+				<div class="news box">
+
+					<div class="icon-img"><i class="fa fa-comments-o fa-3x"></i></div>
+				
+					<h2>Dernières nouvelles</h2>
+				
+					<p>
+						La création d'une association est nécessaire avant d'envisager tout investissement financier. Il nous faut donc définir des <em>statuts</em> et un <em>règlement intérieur</em>.
+
+						<br /><br />
+
+						Après l'observation du fait que les discussions en groupe entier à ces sujets étaient bien trop longues, un groupe de travail a donc été constitué, autour des personnes suivantes : Keks, <s>Zaza</s>, Bastien, Éric, Koline, TimRocket,<del> Ratich</del>, <del>Niko</del>, LeLukas, Fanny et <del>Belette</del>.<br />
+						Ils se sont engagés quant à leur disponibilité et leur travail actif à ces sujets administratifs d'une importance majeure. Leur but est de déblayer au maximum la question avant de soumettre leur premier jet de texte au reste du groupe.<br />
+						Leurs réunions sont évidemment accessibles à tous (voir section dédiée ci-dessous).<br />
+						Les comptes rendus des réunions Groupe de Travail sont tous uploadés <a href="http://trucfou.pe.hu/forum/viewforum.php?f=18">ici</a>.
+					</p>
+					
+					<p>
+						Nous devons (entre autres considérations sémantiques) aussi trouver un <a href="forum/viewtopic.php?f=7&t=3">nom à notre association</a> !
+					</p>
+
+				</div>
+
+				<div class="calendar box">
+
+					<div class="icon-img"><i class="fa fa-calendar fa-3x"></i></div>
+
+					<h2>Prochaines dates</h2>
+
+					<div class="event">
+						<ul class="event-titre">
+							<li class="event-quand"><i class="fa fa-clock-o fa-fw"></i> 12 / 01 / 2017</li>
+							<li class="event-quoi"><i class="fa fa-check fa-fw"></i> Grand Groupe <em>(réu plénière)</em></li>
+							<li class="event-ou"><i class="fa fa-map-marker fa-fw"></i> Annonciade</li>
+						</ul>
+
+						<p>
+							La prochaine réunion Grand Groupe aura lieu à une date encore inconnue, située entre le 10 et le 27 du mois de janvier 2017, dans un lieu qui reste encore à définir. Soyez motivés, soyez chatoyants et donnez vos disponibilités sur <a href="https://framadate.org/vlltexDcPDfH5pmkJcKylArP/admin">ce framazob !</a>
+
+							<br /><br />
+
+							<u><em>Pour rappel, voici le sage message de notre cher LeLukas qui exprimait fort justement la pensée de tous : </em> </u><br />
+							<em>« En fait on s'est dit que faire une réunion à 12 c'était pas optireprésentatif et que nous n'en ferions plus à moins de 16 gens. A vos agenda les poulets ! Pensez à modifier vos disponibilités sur le framazobie si vos disponibilités changent. »</em>
+						</p>
+
+					</div>
+
+					<div class="event">
+						<ul class="event-titre">
+							<li class="event-quand"><i class="fa fa-clock-o fa-fw"></i> Mardi prochain</li>
+							<li class="event-quoi"><i class="fa fa-check fa-fw"></i> Groupe de Travail</em></li>
+							<li class="event-ou"><i class="fa fa-map-marker fa-fw"></i> Lieu à définir</li>
+						</ul>
+
+						<p>
+							Les réunions Groupe de Travail ont lieu tous les mardis soirs à 20h dans une salle de la BMC. Tout le monde peut y participer. Pour être sûr du lieu, allez voir <a href="http://trucfou.pe.hu/forum/viewforum.php?f=19">ici</a>.
+						</p>
+
+					</div>
+
+				</div>
+			</div>
 		</section>
 		<?php include_content('bottom'); ?>
 	</body>
